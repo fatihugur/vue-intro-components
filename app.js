24d5251c474a2bb4168bac28b5bf8da2e954e6b4
Fatih Ugur <fatihugur@gmail.com>
2021-01-21 15:11:38 +0100

    component called in html, data and template updated in component

--- app.js
+++ app.js
@@ -4,12 +4,6 @@ const app = Vue.createApp({
       detailsAreVisible: false,
       friends: [
         {
-          id: "manuel",
-          name: "Manuel Lorenz",
-          phone: "01234 5678 991",
-          email: "manuel@localhost.com",
-        },
-        {
           id: "julie",
           name: "Julie Jones",
           phone: "09876 543 221",
@@ -26,9 +20,28 @@ const app = Vue.createApp({
 });
 
 app.component("friend-contact", {
-  template: ` `,
+  template: `
+        <li>
+          <h2>{{ friend.name }}</h2>
+          <button @click="toggleDetails">
+            {{detailsAreVisible ? 'Hide' : 'Show' }} Details
+          </button>
+          <ul v-if="detailsAreVisible">
+            <li><strong>Phone:</strong> {{ friend.phone }}</li>
+            <li><strong>Email:</strong> {{ friend.email }}</li>
+          </ul>
+        </li>
+   `,
   data() {
-    return { detailsAreVisible: false };
+    return {
+      detailsAreVisible: false,
+      friend: {
+        id: "manuel",
+        name: "Manuel Lorenz",
+        phone: "01234 5678 991",
+        email: "manuel@localhost.com",
+      },
+    };
   },
   methods: {
     toggleDetails() {
